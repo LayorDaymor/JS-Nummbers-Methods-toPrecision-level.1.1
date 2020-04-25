7259c125429fd2f577d4421bf220e04e9883f3a8
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 19:04:32 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let a = 15.16698
+
+console.log(a.toPrecision(2));
+console.log(a.toPrecision(3));
+console.log(a.toPrecision(5));
+
+let b = 7.78714
+console.log(b.toPrecision(2));
+console.log(b.toPrecision(3));
+console.log(b.toPrecision(5));
+
+let c=12.3;
+console.log(c.toPrecision(2));
+console.log(c.toPrecision(3));
+console.log(c.toPrecision(5));
+
+let f=321;
+console.log(f.toPrecision(2));
+console.log(f.toPrecision(3));
+console.log(f.toPrecision(5));
